095423b53f73978eb9c8f987aee57cf26d964dd2
christian bondzie <christian.zap@hotmail.fr>
2025-03-19 18:58:20 +0100

    Creating player model

---
+++ app/controllers/api/v1/players_controller.rb
@@ -0,0 +1,11 @@
+class Api::V1::PlayersController < ApplicationController
+
+  def index
+  end
+
+  def create
+  end
+
+  def update
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -3,4 +3,10 @@ Rails.application.routes.draw do
 
   # Defines the root path route ("/")
   # root "articles#index"
+  namespace :api do
+    namespace :v1 do
+      resources :players, only: %i[create index update]
+    end
+  end
+
 end
