9237c390d887aeecd645fb298322c657f3b55fd3
christian bondzie <christian.zap@hotmail.fr>
2025-04-21 14:03:38 +0200

    Setting base players controller

--- app/controllers/api/v1/players_controller.rb
+++ app/controllers/api/v1/players_controller.rb
@@ -1,11 +1,32 @@
 class Api::V1::PlayersController < ApplicationController
 
   def index
+    @players = Player.all
+    render json: {players: @players }
   end
 
   def create
+    @player = Player.new(name: params[:name])
+    if @player.save
+      render json: { player: @player }, status: :created
+    else
+      render json: { errors: @player.errors.full_messages }, status: :unprocessable_entity
+    end
   end
 
   def update
+    @player = Player.find(id: params[:id])
+
+    if @player.update(players_params)
+      render json: { player: @player, message: "Les informations du joueur ont bien été modifiée" }, status: :ok
+    else
+      render json: { errors: @player.errors.full_messages}, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def players_params
+    params.require(:player).permit(:name)
   end
 end
