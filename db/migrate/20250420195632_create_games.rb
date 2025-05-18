9c9b18b8b326f8fdac9bdd48a8aae35763d3fcf4
christian bondzie <christian.zap@hotmail.fr>
2025-05-18 16:54:34 +0200

    Cngement sur les migrations

--- db/migrate/20250420195632_create_games.rb
+++ db/migrate/20250420195632_create_games.rb
@@ -3,8 +3,8 @@ class CreateGames < ActiveRecord::Migration[7.0]
     create_table :games do |t|
       t.integer :status, default: 0, null: false
       t.integer :stopped_by, null: true
-      t.json :players, default: [], null: false
       t.json :winnners, default: [], null: true
+      t.json :loosers, default: [], null: true
       t.datetime :end_date, null: true
 
       t.timestamps
--- db/migrate/20250503211329_create_pledges.rb
+++ db/migrate/20250503211329_create_pledges.rb
@@ -3,9 +3,9 @@ class CreatePledges < ActiveRecord::Migration[7.0]
     create_table :pledges do |t|
       t.integer :status, default: 0
       t.string :description, null: false
+      t.integer :target_id, null: true
       t.references :player, null: false, foreign_key: true
       t.references :game, null: false, foreign_key: true, null: true
-      t.integer :target_id, null: true
 
       t.timestamps
     end
